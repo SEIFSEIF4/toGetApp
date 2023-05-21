bd81c47501e554ac24cf88057eb6b3fdede05935
SEIFSEIF4 <89165147+SEIFSEIF4@users.noreply.github.com>
2023-05-21 11:48:08 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  push,
+  onValue,
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+
+const appSettings = {
+  databaseURL: "https://togetapp-c9973-default-rtdb.firebaseio.com/",
+};
+
+//Real Time DB
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const groceryInDB = ref(database, "grocery");
+
+//APP JS
+const inputFieldEl = document.getElementById("input-field");
+const addButtonEl = document.getElementById("add-button");
+const shoppingListEl = document.getElementById("shopping-list");
+
+addButtonEl.addEventListener("click", function () {
+  let inputValue = inputFieldEl.value;
+
+  push(groceryInDB, inputValue);
+
+  clearInputFieldEl();
+
+  appendItemToShoppingListEl(inputValue);
+});
+
+
+onValue(groceryInDB, function(snapshot) {
+  let itemsArray = Object.values(snapshot.val()) 
+
+  for (let i = 0; i < itemsArray.length; i++) {
+      appendItemToShoppingListEl(itemsArray[i])
+  }
+})
+
+function clearInputFieldEl() {
+  inputFieldEl.value = "";
+}
+
+function appendItemToShoppingListEl(itemValue) {
+  shoppingListEl.innerHTML += `<li>${itemValue}</li>`;
+}
